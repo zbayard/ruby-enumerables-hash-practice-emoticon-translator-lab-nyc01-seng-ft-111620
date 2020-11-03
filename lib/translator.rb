ba55440966029ec9f2f6b3a183c2a9383088629f
Zach Bayard <zbayard@gmail.com>
2020-11-03 22:29:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,16 +20,9 @@ require 'yaml'
 require 'pry'
 
 def load_library(path)
-  emoji_hash = {}
-  emoji_hash["get_meaning"] = {}
-  emoji_hash["get_emoticon"] = {}
-  emoji = YAML.load_file(path)
-  emoji.each do |word, emojis|
-    emoji_hash["get_meaning"][emojis[1]] = word
-    emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
-  end
-  emoji_hash
-end
+  emoticons.each do |key, value|
+      emoticon_lib[key][:english => value[0], :japanese => value[1]]
+    end
 
 def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
