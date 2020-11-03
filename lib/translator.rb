38cba19e2de0d6286531db6c0d0c290b5f1af56b
Zach Bayard <zbayard@gmail.com>
2020-11-03 22:14:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,55 +1,15 @@
 # require lib/emoticons.yml # require modules here
 
 
-# require "yaml"
-# require "pry"
-
-require "lib/emoticons.yml"
 
 def load_library
   # code goes here
-def load_library(path)
-  emoji_hash = {}
-  emoji_hash["get_meaning"] = {}
-  emoji_hash["get_emoticon"] = {}
-  emoji = YAML.load_file(path)
-  emoji.each do |word, emojis|
-    emoji_hash["get_meaning"][emojis[1]] = word
-    emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
-  end
-  emoji_hash
 end
 
 def get_japanese_emoticon
   # code goes here
-def get_japanese_emoticon(path, emoticon)
-  emoji = load_library(path)
-  if emoji["get_emoticon"].has_key?(emoticon)
-    emoji["get_emoticon"][emoticon]
-  else
-    return "Sorry, that emoticon was not found"
-  end
 end
 
 def get_english_meaning
   # code goes here
 end
-def get_english_meaning(path, emoticon)
-  emoji = load_library(path)
-  if emoji["get_meaning"].has_key?(emoticon)
-    emoji["get_meaning"][emoticon]
-  else
-    return "Sorry, that emoticon was not found"
-  end
-end
-# def load_library
-#   # code goes here
-# end
-#
-# def get_japanese_emoticon
-#   # code goes here
-# end
-#
-# def get_english_meaning
-#   # code goes here
-# end
